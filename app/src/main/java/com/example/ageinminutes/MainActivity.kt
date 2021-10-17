724a0306e5978023a0076974458e1b241b4b9498
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-17 20:26:04 -0300

    adding time to app

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.time.minutes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +28,25 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(java.util.Calendar.MONTH)
         val dayOfMonth = myCalendar.get(java.util.Calendar.DAY_OF_MONTH)
         val textDate : AppCompatTextView = findViewById(R.id.date)
-        val hintSelecteddate : AppCompatTextView = findViewById(R.id.title_selected_date)
+        val hintSelectedDate : AppCompatTextView = findViewById(R.id.title_selected_date)
+        val hintMinutesDisplayed : AppCompatTextView = findViewById(R.id.hint_minutes)
+        val resultMinute : AppCompatTextView = findViewById(R.id.result_minutes)
 
 
         DatePickerDialog(this, DatePickerDialog.OnDateSetListener
         { v, i, i2, i3 ->
-            textDate.text = "$i3/$i2/$i"
-            hintSelecteddate.visibility = View.VISIBLE
+
+            val selectedDate = "$i3/${i2+1}/$i"
+
+            textDate.text = selectedDate
+            hintSelectedDate.visibility = View.VISIBLE
+
+            val simpleDateFormat = SimpleDateFormat("dd/mm/yyyy")
+            val theDate = simpleDateFormat.parse(selectedDate)
+            val selectedDateInMinutes = theDate!!.time / 60000
+
+            resultMinute.text = selectedDateInMinutes.toString()
+            hintMinutesDisplayed.visibility = View.VISIBLE
         }, year, month, dayOfMonth).show()
     }
 }
